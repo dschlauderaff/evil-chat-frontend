48d4fd628c7dc753e4247ac7b5b9da73b5e40fcd
Dave Schlauderaff <dschlaud@gmail.com>
2017-12-22 10:46:48 -0600

    creates rails portion of actioncable and user validation for the channel

--- app/channels/application_cable/connection.rb
+++ app/channels/application_cable/connection.rb
@@ -1,4 +1,10 @@
 module ApplicationCable
   class Connection < ActionCable::Connection::Base
+    identified_by :current_user
+
+    def Connection
+      self.current_user = request.session.fetch("username", nil)
+      reject_unauthorized_connection unless current_user
+    end
   end
 end
